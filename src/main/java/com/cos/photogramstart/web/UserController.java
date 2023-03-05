39db893ef86496029836f1e76b4d4d2b028bd426
kimyeaseul <adgjl3151@hanmail.net>
2023-03-05 20:48:35 +0900

    Add : security 세션 정보 접근하기

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,5 +1,9 @@
 package com.cos.photogramstart.web;
 
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +17,14 @@ public class UserController {
     }
 
     @GetMapping("/user/update/{id}")
-    public String update(@PathVariable int id){
+    public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        // 쉽게 찾는법
+        System.out.println("세션정보" + principalDetails.getUser());
+
+        // 힘들게 세션 정보 찾는법...
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication(); // Authentication
+        PrincipalDetails mPrincipalDetails = (PrincipalDetails) auth.getPrincipal();
+        System.out.println("직접 찾은 세션 정보 " + mPrincipalDetails.getUser());
         return "user/update";
     }
 }

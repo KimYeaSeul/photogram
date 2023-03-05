c03ddb502c8b57d5269cb3a9bcbe98ec5fa2d2ca
kimyeaseul <adgjl3151@hanmail.net>
2023-03-05 18:15:45 +0900

    Add : 회원가입 완료, response Dto 추가

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.cos.photogramstart.handler;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.util.Script;
+import com.cos.photogramstart.web.dto.CMRespDto;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +15,12 @@ import java.util.Map;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class)
-    public Map<String, String> validationException(CustomValidationException e){
-        return e.getErrorMap();
+    public String validationException(CustomValidationException e){
+
+        // 1. 클라이언트에게 응답할 때는 Script가 좋음. -> 브라우저가 응답 받음
+        // 2. Ajax 통신 - CMRespDto 사용 -> 개발자가 응답 받음
+        // 3. Android 통신 - CMRespDto 사용 -> 개발자가 응답 받음
+//        return new CMRespDto(-1, e.getMessage(), e.getErrorMap());
+        return Script.back(e.getErrorMap().toString());
     }
 }
